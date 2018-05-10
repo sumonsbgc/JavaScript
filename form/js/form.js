a77280f162b3abdea994de7d10b4786bcf0bda49
Mohammad Sumon <sumonsbgc@gmail.com>
2018-05-11 04:07:35 +0600

    just formating

--- form/js/form.js
+++ form/js/form.js
@@ -1,4 +1,3 @@
-
 var form = document.getElementById("signup_form");
 // var file = form.user_profile.value;
 var form = document.forms.signup_form;
@@ -8,12 +7,12 @@ var email = form.signup_email.value;
 var pass = form.signup_pass.value;
 var pass_again = form.signup_pass_again.value;
 
-function checkingInputValue(){       
+function checkingInputValue() {
     if (("" == fname || undefined == fname || NaN == fname) &&
         ("" == lname || undefined == lname || NaN == lname) &&
         ("" == email || undefined == email || NaN == email) &&
-        ("" == pass  || undefined == pass  || NaN == pass) &&
-        ("" == pass_again || undefined == pass_again || NaN == pass_again)){
+        ("" == pass || undefined == pass || NaN == pass) &&
+        ("" == pass_again || undefined == pass_again || NaN == pass_again)) {
         alert("All The Field are empty. Please Fill The Fields");
     }
 }
