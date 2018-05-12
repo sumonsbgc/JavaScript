ea2ff9178612c4624551c159fa06490c6fd94088
Mohammad Sumon <sumonsbgc@gmail.com>
2018-05-13 03:36:24 +0600

    Try to work with ajax

--- form/js/form.js
+++ form/js/form.js
@@ -1,23 +1,24 @@
-var form = document.getElementById("signup_form");
-// var file = form.user_profile.value;
-var form = document.forms.signup_form;
-var fname = form.signup_fname.value;
-var lname = form.signup_lname.value;
-var email = form.signup_email.value;
-var pass = form.signup_pass.value;
-var pass_again = form.signup_pass_again.value;
-
-function checkingInputValue() {
-    if (("" == fname || undefined == fname || NaN == fname) &&
-        ("" == lname || undefined == lname || NaN == lname) &&
-        ("" == email || undefined == email || NaN == email) &&
-        ("" == pass || undefined == pass || NaN == pass) &&
-        ("" == pass_again || undefined == pass_again || NaN == pass_again)) {
-        alert("All The Field are empty. Please Fill The Fields");
-    }
-}
+// var form = document.getElementById("signup_form");
+// // var file = form.user_profile.value;
+// var form = document.forms.signup_form;
+// var fname = form.signup_fname.value;
+// var lname = form.signup_lname.value;
+// var email = form.signup_email.value;
+// var pass = form.signup_pass.value;
+// var pass_again = form.signup_pass_again.value;
 
 
-form.signup_submit.addEventListener('keypress', function (e) {
-    console.log(e);
-});
+// function checkingInputValue() {
+//     if (("" == fname || undefined == fname || NaN == fname) &&
+//         ("" == lname || undefined == lname || NaN == lname) &&
+//         ("" == email || undefined == email || NaN == email) &&
+//         ("" == pass || undefined == pass || NaN == pass) &&
+//         ("" == pass_again || undefined == pass_again || NaN == pass_again)) {
+//         alert("All The Field are empty. Please Fill The Fields");
+//     }
+// }
+
+
+// form.signup_submit.addEventListener('keypress', function (e) {
+//     console.log(e);
+// });
---
+++ form/output.php
@@ -0,0 +1,5 @@
+<?php
+
+if(isset($_POST['signup_submit'])){
+    print_r($_POST);
+}
