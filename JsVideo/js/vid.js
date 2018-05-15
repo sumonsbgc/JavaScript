9968cc8b95d6c9d150357a7176f4be038ba9d806
Mohammad Sumon <sumonsbgc@gmail.com>
2018-05-16 05:06:08 +0600

    commiting some new files

--- JsVideo/js/vid.js
+++ JsVideo/js/vid.js
@@ -1,24 +1,29 @@
-var video = document.getElementById("vid_player"),
+var video = document.querySelector("video"),
     play  = document.querySelector(".play"),
     pause = document.querySelector(".pause");
-
-play.addEventListener("click", function(){
-    video.play();
-});
-
-pause.addEventListener("click", function(){
-    video.pause();
-});
+video.removeAttribute("autoplay");
+video.autoplay = false;
 
 
-var video = $("#vid_player"),
-    play  = $(".play"),
-    pause = $(".pause");
+if (video.paused) {
+    video.addEventListener("click", function () {
+        video.play();
+    });    
+} else {
+    video.addEventListener("click", function () {
+        video.pause();
+    });    
+}
 
-play.on("click", function(){
-    video.play();
-});
 
-pause.on("click", function(){
-    video.pause();
-});
+// var video = $("#vid_player"),
+//     play  = $(".play"),
+//     pause = $(".pause");
+
+// play.on("click", function(){
+//     video.play();
+// });
+
+// pause.on("click", function(){
+//     video.pause();
+// });
