87e5931d9465e835fe14f8a67cd481b1cbc0934f
sumonsbgc <sumonsbgc@gmail.com>
2018-06-20 17:01:23 +0600

    creating another map

--- form/js/form.js
+++ form/js/form.js
@@ -1,60 +1,60 @@
-var form        =   document.querySelector("#signup_form");
-var fname       =   "",
-    lname       =   "",
-    email       =   "",
-    pass        =   "",
-    pass_again  =   "";
+// var form        =   document.querySelector("#signup_form");
+// var fname       =   "",
+//     lname       =   "",
+//     email       =   "",
+//     pass        =   "",
+//     pass_again  =   "";
 
 
-form.signup_fname.addEventListener("blur", function(e){
-    if(this.value){
-        this.nextElementSibling.innerText = "";
-        this.nextElementSibling.style.display = "none";
-    }else{
-        this.nextElementSibling.style.display = "block";
-        this.nextElementSibling.innerText = "You can't leave this field in empty!";
-    }
-});
+// form.signup_fname.addEventListener("blur", function(e){
+//     if(this.value){
+//         this.nextElementSibling.innerText = "";
+//         this.nextElementSibling.style.display = "none";
+//     }else{
+//         this.nextElementSibling.style.display = "block";
+//         this.nextElementSibling.innerText = "You can't leave this field in empty!";
+//     }
+// });
 
-form.signup_fname.addEventListener("keyup", function(){
-    if(this.value){
-        this.nextElementSibling.innerText = "";
-        this.nextElementSibling.style.display = "none";
-    }
-});
+// form.signup_fname.addEventListener("keyup", function(){
+//     if(this.value){
+//         this.nextElementSibling.innerText = "";
+//         this.nextElementSibling.style.display = "none";
+//     }
+// });
 
-form.signup_submit.addEventListener("click", function(e){    
+// form.signup_submit.addEventListener("click", function(e){    
     
-    e.preventDefault();
+//     e.preventDefault();
 
-    fname = form.signup_fname.value;
-    lname = form.signup_lname.value;
-    email = form.signup_email.value;
-    pass  = form.signup_pass.value;
-    pass_again = form.signup_pass_again.value;
+//     fname = form.signup_fname.value;
+//     lname = form.signup_lname.value;
+//     email = form.signup_email.value;
+//     pass  = form.signup_pass.value;
+//     pass_again = form.signup_pass_again.value;
 
-    if(fname && lname && email && pass && pass_again){
-        alert("Succeed");
-        document.querySelector(".validate_error").innerText = "";
-        document.querySelector(".validate_error").style.display = "none";
-    }else{
-        document.querySelector(".validate_error").innerText = "All the fields are empty! Please fill the required fields";
-        document.querySelector(".validate_error").style.display = "block";
-    }
+//     if(fname && lname && email && pass && pass_again){
+//         alert("Succeed");
+//         document.querySelector(".validate_error").innerText = "";
+//         document.querySelector(".validate_error").style.display = "none";
+//     }else{
+//         document.querySelector(".validate_error").innerText = "All the fields are empty! Please fill the required fields";
+//         document.querySelector(".validate_error").style.display = "block";
+//     }
     
-    if(!fname){
-        alert("Name Field Empty!");
-    }else if (!lname) {
-        alert("This Field Is empty!");
-    }else if (!email) {
-        alert("This Field Is empty!");
-    }else if (!pass) {
-        alert("This Field Is empty!");
-    }else if(!pass_again){
-        alert("This Field Is empty!");
-    }
+//     if(!fname){
+//         alert("Name Field Empty!");
+//     }else if (!lname) {
+//         alert("This Field Is empty!");
+//     }else if (!email) {
+//         alert("This Field Is empty!");
+//     }else if (!pass) {
+//         alert("This Field Is empty!");
+//     }else if(!pass_again){
+//         alert("This Field Is empty!");
+//     }
 
-});
+// });
 
 
 
--- form/output.php
+++ form/output.php
@@ -1,6 +1,35 @@
 <?php
 
-if(isset($_POST['signup_submit'])){
-    echo "<pre>";
-    print_r($_POST);
+if(isset($_FILES["user_profile"]))
+{
+
+    $fname          = $_POST['signup_fname'];
+    $lname          = $_POST['signup_lname'];
+    $email          = $_POST['signup_email'];
+    $pass           = $_POST['signup_pass'];
+    $repeat_pass    = $_POST['signup_pass_again'];
+
+    $csvFile = $_FILES["user_profile"]["name"];
+    $csvTempName = $_FILES["user_profile"]["tmp_name"];
+ 
+    echo($fname.", ". $lname.", ". $email.", ".$csvFile.", ". $csvTempName);
+    printf("Your name is %s %s and Your Email is %s. You uploaded ");
+
+
+    
+
+    // echo $csvTempName;
+    // echo $csvFile;
+
+
+    // $data = json_encode($_POST);
+    // print_r($data);
+
+    // $fname = $_POST['signup_fname'];
+    // $lname = $_POST['signup_lname'];
+    // $email = $_POST['signup_email'];
+
+    // // echo "Hello " . $fname . " " . $lname . ", Is this your email => " . $email;
+    // printf("Hello %s %s, Is this your email? => %s", $fname, $lname, $email);
+    
 }
